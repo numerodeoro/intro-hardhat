68111420a6462a75d1624766abf72ca05181674c
numerodeoro <numerodeoro@live.com>
2024-12-12 19:41:24 -0300

    configurando el hardhat

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,11 +2,18 @@ import "dotenv/config";
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
-console.log("correr el script de hardhat");
-console.log("mi variable es: ", process.env.HELLO);
+const ALCHEMY_API_KEY_SEPOLIA = process.env.ALCHEMY_API_KEY_SEPOLIA;
 
 const config: HardhatUserConfig = {
   solidity: "0.8.28",
+  networks: {
+    SEPOLIA: {
+      url : 'https://eth-sepolia.g.alchemy.com/v2/${ALCHEMY_API_KEY_SEPOLIA}',
+      accounts: [process.env.PRIVATE_KEY_SEPOLIA || ""]
+    }
+  },
+  etherscan: {},
+
 };
 
 export default config;
