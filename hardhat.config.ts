fddf05dc5eb2eee0bdc424353d68528ff8caa881
numerodeoro <numerodeoro@live.com>
2024-12-13 15:35:51 -0300

    terminando el hardhat.config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,17 +2,22 @@ import "dotenv/config";
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
-const ALCHEMY_API_KEY_SEPOLIA = process.env.ALCHEMY_API_KEY_SEPOLIA;
+const ALCHEMY_API_KEY_SEPOLIA = process.env.ALCHEMY_API_KEY_SEPOLIA || "";
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || "";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.28",
-  networks: {
-    SEPOLIA: {
+  networks:{
+    sepolia: {
       url : 'https://eth-sepolia.g.alchemy.com/v2/${ALCHEMY_API_KEY_SEPOLIA}',
       accounts: [process.env.PRIVATE_KEY_SEPOLIA || ""]
     }
   },
-  etherscan: {},
+  etherscan: {
+    apiKey: {
+      sepolia: ETHERSCAN_API_KEY
+    } 
+  }
 
 };
 
