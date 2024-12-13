d41d23741abfa1e74d0de510100288efe20f702e
numerodeoro <numerodeoro@live.com>
2024-12-13 18:26:49 -0300

    deployments and verifications

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -8,8 +8,8 @@ const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || "";
 const config: HardhatUserConfig = {
   solidity: "0.8.28",
   networks:{
-    sepolia: {
-      url : 'https://eth-sepolia.g.alchemy.com/v2/${ALCHEMY_API_KEY_SEPOLIA}',
+    sepolia:{
+      url: `https://eth-sepolia.g.alchemy.com/v2/${ALCHEMY_API_KEY_SEPOLIA}`,
       accounts: [process.env.PRIVATE_KEY_SEPOLIA || ""]
     }
   },
